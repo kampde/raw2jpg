12275bf430b0d9ccbcd8bbaac7cf9ff51cef633b
Carlos Campderrós <carlos@campderros.com>
2016-09-30 22:11:25 +0200

    move all logic to rawlib

--- rawlib.py
+++ rawlib.py
@@ -1,3 +1,7 @@
+import io
+import os
+
+from PIL import Image
 from gi import require_version
 require_version('GExiv2', '0.10')
 from gi.repository import GExiv2
@@ -22,3 +26,26 @@ def get_metadata(path):
         exif.open_path(path)
     return exif
 
+
+def miniature(nef):
+    jpg = (os.path.dirname(nef) or '.') + '/jpg/' + '.'.join(os.path.basename(nef).split('.')[:-1]) + '.from_raw.jpg'
+    try:
+        os.mkdir(os.path.dirname(jpg))
+    except FileExistsError:
+        pass
+
+    print('{} => {}'.format(nef, jpg))
+
+    exif = get_metadata(path=nef)
+    orientation = exif.get_orientation()
+
+    im_bytes = io.BytesIO(get_preview_bytes(exif))
+
+    im = Image.open(im_bytes)
+    width, height = im.size
+    im.thumbnail((1920, 1920))
+    im.save(jpg)
+    exif2 = get_metadata(path=jpg)
+    exif2.set_orientation(orientation)
+
+    exif2.save_file(jpg)
